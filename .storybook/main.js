7d05e68867f4a5285d044a5322565860241f08d0
Machisté N. Quintana <mnquintana@users.noreply.github.com>
2021-04-18 22:53:16 -0400

    Minimal repro on Storybook 6.2

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../stories/*.stories.@(ts|tsx|js|jsx|mdx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-}
+  stories: ["../stories/*.stories.@(ts|tsx|js|jsx|mdx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+};
